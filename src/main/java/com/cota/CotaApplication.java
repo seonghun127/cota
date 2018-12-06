c12a0765988603f307db515cc0bbce83cd6202ff
김성훈 <seonghun127@gmail.com>
2018-12-06 22:35:24 +0900

    first commit

---
+++ src/main/java/com/cota/CotaApplication.java
@@ -0,0 +1,12 @@
+package com.cota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotaApplication.class, args);
+	}
+}
