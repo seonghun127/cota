4f45a51186d93d044ab7e417fe2ac6c8a453685d
김성훈 <seonghun127@gmail.com>
2018-12-12 17:17:41 +0900

    update file upload method

---
+++ src/main/java/com/cota/controller/FileController.java
@@ -0,0 +1,256 @@
+package com.cota.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cota.util.MediaUtils;
+import com.cota.util.UploadFileUtils;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class FileController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+	
+	// 로컬 경로
+	//private static final String UPLOAD_PATH = 
+	//		"/Users/gimseonghun/Documents/springboot/cota/src/main/resources/static/images/";
+	
+	// 운영 서버 경로
+	private static final String UPLOAD_PATH = "/home/ec2-user/app/img/";
+	
+	
+    // ------------------------------------------------------------------------------ //
+    
+    /**
+     * 파일 업로드 이벤트 처리 메소드 (original)
+     * @param file
+     * @return
+     * @throws Exception
+     */
+    @CrossOrigin
+	@PostMapping(value="/upload", produces = "text/plain;charset=UTF-8")
+	public ResponseEntity<String> uploadImage(MultipartFile file) throws Exception {
+		
+		logger.info("uploadImage starts");
+		logger.info("originalName:" + file.getOriginalFilename());
+		logger.info("size:" + file.getSize());
+		logger.info("contentType:" + file.getContentType());
+		
+		//HttpStatus.CREATED : 리소스가 정상적으로 생성되었다는 상태코드.
+		//return new ResponseEntity<>(file.getOriginalFilename(), HttpStatus.CREATED);
+		return new ResponseEntity<>(
+				UploadFileUtils.uploadFile(UPLOAD_PATH,
+												file.getOriginalFilename(),
+												file.getBytes()),
+						HttpStatus.CREATED);
+	}
+	
+	// ------------------------------------------------------------------------------ //
+    
+    /**
+     * 저장된 파일(사진)을 화면에 보여주기 (original)
+     * @param fileName
+     * @return
+     * @throws Exception
+     */
+  	@GetMapping("displayFile")
+  	public ResponseEntity<byte[]> displayFile(String fileName) throws Exception {
+  		
+  		InputStream in = null;
+  		ResponseEntity<byte[]> entity = null;
+  		
+  		logger.info("File name: " + fileName);
+  		
+  		try {
+  			String formatName = fileName.substring(fileName.lastIndexOf(".")+1);
+  			
+  			MediaType mType = MediaUtils.getMediaType(formatName);
+  			
+  			HttpHeaders headers = new HttpHeaders();
+  			
+  			in = new FileInputStream(UPLOAD_PATH + fileName);
+  			
+  			if(mType != null) {
+  				headers.setContentType(mType);
+  			}else {
+  				fileName = fileName.substring(fileName.indexOf("_")+1);
+  				headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+  				headers.add("Content-Disposition", "attachment; filename=\"" 
+  							+ new String(fileName.getBytes("UTF-8"), "ISO-8859-1") + "\"");
+  			}// else
+  			
+  			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+  			
+  		} catch(Exception e) {
+  			e.printStackTrace();
+  			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+  		} finally {
+  			in.close();
+  		}
+  		
+  		return entity;
+  	}// displayFile
+  	
+  	// ------------------------------------------------------------------------------ //
+  	
+  	/**
+  	 * multipartFile로 이미지를 받고 썸네일 이미지로 변환 후 리턴 메소드
+  	 * @param file
+  	 * @return
+  	 * @throws Exception
+  	 */
+  	@PostMapping(value = "displayFile2", produces = "text/plain;charset=UTF-8")
+  	public ResponseEntity<byte[]> displayFile2(MultipartFile file) throws Exception {
+  		
+  		InputStream in = null;
+  		ResponseEntity<byte[]> entity = null;
+  		String fileName;
+  		
+  		try {
+  			fileName = UploadFileUtils.uploadFile(UPLOAD_PATH,
+					file.getOriginalFilename(),
+					file.getBytes());
+  			
+  			logger.info("File name: " + fileName);
+  			
+  			String formatName = fileName.substring(fileName.lastIndexOf(".")+1);
+  			
+  			MediaType mType = MediaUtils.getMediaType(formatName);
+  			
+  			HttpHeaders headers = new HttpHeaders();
+  			
+  			in = new FileInputStream(UPLOAD_PATH + fileName);
+  			
+  			if(mType != null) {
+  				headers.setContentType(mType);
+  			}else {
+  				fileName = fileName.substring(fileName.indexOf("_")+1);
+  				headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+  				headers.add("Content-Disposition", "attachment; filename=\"" 
+  							+ new String(fileName.getBytes("UTF-8"), "ISO-8859-1") + "\"");
+  			}// else
+  			
+  			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+  			
+  		} catch(Exception e) {
+  			e.printStackTrace();
+  			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+  		} finally {
+  			in.close();
+  		}
+  		
+  		return entity;
+  	}
+  	
+  	// ------------------------------------------------------------------------------ //
+	
+  	/**
+  	 * 이미지 파일 리턴 메소드 예제 (미완성)
+  	 * @param fileName
+  	 * @return
+  	 * @throws IOException
+  	 */
+	@GetMapping(value = "/getImage/{fileName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImage(@PathVariable String fileName) throws IOException {
+
+        ClassPathResource imgFile = new ClassPathResource("/static/images/"+fileName);
+        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(bytes);
+    }
+    
+    // ------------------------------------------------------------------------------ //
+	
+    /**
+     * 이미지 업로드 메소드 예제 (미완성)
+     * @param files
+     * @return
+     * @throws Exception
+     */
+	@CrossOrigin
+	@GetMapping(value = "/insertImage", produces = MediaType.IMAGE_JPEG_VALUE)
+	private ResponseEntity<byte[]> boardInsertProc(@RequestPart MultipartFile files) throws Exception{
+	
+	    String fileName = files.getOriginalFilename(); 
+	    String fileNameExtension = FilenameUtils.getExtension(fileName).toLowerCase(); 
+	    File destinationFile; 
+	    String destinationFileName; 
+	    String file;
+	    //String fileUrl = "/Users/gimseonghun/Documents/springboot/cota/src/main/resources/static/images/";
+	    String fileUrl = "/home/ec2-user/app/img/";
+	 
+	    do { 
+	    	destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + fileNameExtension;
+	        file = fileUrl + destinationFileName;
+	        destinationFile = new File(file); 
+	    } while (destinationFile.exists()); 
+	     
+	    destinationFile.getParentFile().mkdirs(); 
+	    files.transferTo(destinationFile);
+	
+	    InputStream in = new FileInputStream(file);
+	    byte[] bytes2 = IOUtils.toByteArray(in);
+	     
+	    return ResponseEntity
+	            .ok()
+	            .contentType(MediaType.IMAGE_JPEG)
+	            .body(bytes2);
+	}
+	 
+	// ------------------------------------------------------------------------------ //
+	
+	/**
+	 * 
+	 */
+	/*
+	@CrossOrigin
+	@PostMapping("/insertImage")
+    private String boardInsertProc(@RequestPart MultipartFile files) throws Exception{
+		
+        String fileName = files.getOriginalFilename(); 
+        String fileNameExtension = FilenameUtils.getExtension(fileName).toLowerCase(); 
+        File destinationFile; 
+        String destinationFileName; 
+        String file;
+        String fileUrl = "/static/images/";
+        
+        do { 
+            destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + fileNameExtension;
+            file = fileUrl + destinationFileName;
+            destinationFile = new File(file); 
+        } while (destinationFile.exists()); 
+        
+        destinationFile.getParentFile().mkdirs(); 
+        files.transferTo(destinationFile);
+
+        return file;
+    } */
+
+}
--- src/main/java/com/cota/controller/PostsController.java
+++ src/main/java/com/cota/controller/PostsController.java
@@ -1,36 +1,14 @@
 package com.cota.controller;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.cota.dto.PostsSaveDto;
 import com.cota.service.PostsService;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @RestController
 @AllArgsConstructor
@@ -48,89 +26,5 @@ public class PostsController {
 	public Long savePosts(@RequestBody PostsSaveDto dto) {
 		return postsService.save(dto);
 	}
-	
-	@CrossOrigin
-	@GetMapping(value = "/insertImage", produces = MediaType.IMAGE_JPEG_VALUE)
-    private ResponseEntity<byte[]> boardInsertProc(@RequestPart MultipartFile files) throws Exception{
-		
-        String fileName = files.getOriginalFilename(); 
-        String fileNameExtension = FilenameUtils.getExtension(fileName).toLowerCase(); 
-        File destinationFile; 
-        String destinationFileName; 
-        String file;
-        //String fileUrl = "/Users/gimseonghun/Documents/springboot/cota/src/main/resources/static/images/";
-        String fileUrl = "/home/ec2-user/app/img/";
-        
-        do { 
-            destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + fileNameExtension;
-            file = fileUrl + destinationFileName;
-            destinationFile = new File(file); 
-        } while (destinationFile.exists()); 
-        
-        destinationFile.getParentFile().mkdirs(); 
-        
-        files.transferTo(destinationFile);
 
-        //ClassPathResource imgFile = new ClassPathResource("/static/images/"+destinationFileName);
-        
-        //byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
-        InputStream in = new FileInputStream(file);
-        byte[] bytes2 = IOUtils.toByteArray(in);
-        
-        return ResponseEntity
-                .ok()
-                .contentType(MediaType.IMAGE_JPEG)
-                .body(bytes2);
-    }
-	
-	@GetMapping(value = "/getImage/{fileName}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> getImage(@PathVariable String fileName) throws IOException {
-
-        ClassPathResource imgFile = new ClassPathResource("/static/images/"+fileName);
-        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
-
-        return ResponseEntity
-                .ok()
-                .contentType(MediaType.IMAGE_JPEG)
-                .body(bytes);
-    }
-	
-	/*
-	@CrossOrigin
-	@PostMapping("/insertImage")
-    private String boardInsertProc(@RequestPart MultipartFile files) throws Exception{
-		
-        String fileName = files.getOriginalFilename(); 
-        String fileNameExtension = FilenameUtils.getExtension(fileName).toLowerCase(); 
-        File destinationFile; 
-        String destinationFileName; 
-        String file;
-        String fileUrl = "/static/images/";
-        
-        do { 
-            destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + fileNameExtension;
-            file = fileUrl + destinationFileName;
-            destinationFile = new File(file); 
-        } while (destinationFile.exists()); 
-        
-        destinationFile.getParentFile().mkdirs(); 
-        files.transferTo(destinationFile);
-
-        return file;
-    } */
-	
-	
-	
-	@NoArgsConstructor
-    @Data
-    private static class UploadAttachmentResponse {
-
-        private String fileName;
-
-        private long fileSize;
-
-        private String fileContentType;
-
-        private String attachmentUrl;
-    }
 }
