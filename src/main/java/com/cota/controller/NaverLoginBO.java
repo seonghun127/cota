806374a0e51f76bf58a03d4481d51e0e692ffdd1
김성훈 <seonghun127@gmail.com>
2019-01-02 21:34:45 +0900

    update naver login

--- src/main/java/com/cota/controller/NaverLoginBO.java
+++ src/main/java/com/cota/controller/NaverLoginBO.java
@@ -49,7 +49,7 @@ public class NaverLoginBO {
 
         /* Callback으로 전달받은 세선검증용 난수값과 세션에 저장되어있는 값이 일치하는지 확인 */
         String sessionState = getSession(session);
-        System.out.println("session is "+sessionState);
+        System.out.println("At getAccessToken session is "+getSession(session));
         if(StringUtils.pathEquals(sessionState, state)){
 
             OAuth20Service oauthService = new ServiceBuilder()
